5eb33f1ea14f9098a2cb30dc62ef03a3b454ff0d
Abhi Success <125504285+abhisuccess@users.noreply.github.com>
2024-08-19 16:12:10 +0530

    Add files via upload
    HTTP/2 servers based on kubernetes ( Load balancing using round robin )

---
+++ server2.py
@@ -0,0 +1,19 @@
+import asyncio
+from aiohttp import web
+import logging
+
+# Configure logging
+logging.basicConfig(filename='/home/abhi1/server2.log',  # Path to your log file
+                    level=logging.INFO,  # Log level
+                    format='%(asctime)s - %(levelname)s - %(message)s')  # Log format
+
+async def handle(request):
+    logging.info('Received request at %s', request.path)  # Log each request
+    await asyncio.sleep(1)  # Simulate some processing time
+    return web.Response(text="Hello from Server 2")
+
+app = web.Application()
+app.router.add_get('/hello', handle)
+
+web.run_app(app, port=8082)
+
